c9e4f7b76a5c04fa3eb8eef111bb23c80fb32e10
Raphael Brand <info@raphaelbrand.de>
2021-02-13 17:01:00 +0100

    modeler page inclusions

---
+++ src/pages/ModelerPage.js
@@ -0,0 +1,6 @@
+import BpmnJs from "bpmn-js/dist/bpmn-modeler.development.js";
+import "bpmn-js/dist/assets/diagram-js.css";
+import "bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css";
+
+// export a modeler page component
+// use the modeler to instantiate a modeler instanceof
