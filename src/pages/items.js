dcd2e8ad32669e0e43c07554a19385a83cb7c23e
Raphael Brand <info@raphaelbrand.de>
2021-02-17 20:06:37 +0100

    fix typos in framorks data

--- src/pages/items.js
+++ src/pages/items.js
@@ -3,12 +3,12 @@ const items = [
   "React":"React (also known as React. js or ReactJS) is an open-source, front end, JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications."
  },
 {
-  "Backbone":"js is a JavaScript library with a RESTful JSON interface and is based on the model–view–controller application design paradigm. Backbone is known for being lightweight, as its only hard dependency is on one JavaScript library, Underscore. js, plus jQuery for use of the full library."
+  "Backbone":"Backbone.js is a JavaScript library with a RESTful JSON interface and is based on the model–view–controller application design paradigm. Backbone is known for being lightweight, as its only hard dependency is on one JavaScript library, Underscore. js, plus jQuery for use of the full library."
 },{
   "AngularJS":"AngularJS is a structural framework for dynamic web apps. It lets you use HTML as your template language and lets you extend HTML's syntax to express your application's components clearly and succinctly. ... AngularJS is what HTML would have been, had it been designed for applications."  
 },
 {
-  "Ember.js":"js is an open-source JavaScript web framework, utilizing a component-service pattern. It allows developers to create scalable single-page web applications by incorporating common idioms, best practices, and patterns from other single-page-app ecosystem patterns into the framework."  
+  "Ember.js":"Ember.js is an open-source JavaScript web framework, utilizing a component-service pattern. It allows developers to create scalable single-page web applications by incorporating common idioms, best practices, and patterns from other single-page-app ecosystem patterns into the framework."  
 },
 {
   "Knockout":"Knockout is a JavaScript library that helps you to create rich, responsive display and editor user interfaces with a clean underlying data model. ... Elegant dependency tracking - automatically updates the right parts of your UI whenever your data model changes.",
@@ -17,7 +17,7 @@ const items = [
   "Meteor":"Meteor is a full-stack JavaScript platform for developing modern web and mobile applications. Meteor includes a key set of technologies for building connected-client reactive applications, a build tool, and a curated set of packages from the Node. js and general JavaScript community."
 },
 {
-"Vue.js":"Vue. js (commonly referred to as Vue; pronounced /vjuː/, like \"view\") is an open-source model–view–viewmodel front end JavaScript framework for building user interfaces and single-page applications."  
+"Vue.js":"Vue.js (commonly referred to as Vue; pronounced /vjuː/, like \"view\") is an open-source model–view–viewmodel front end JavaScript framework for building user interfaces and single-page applications."  
 },
 {
 "jQuery":"jQuery is a cross-platform JavaScript library designed to simplify the client-side scripting of HTML. ... It makes things like HTML document traversal and manipulation, event handling, animation, and Ajax much simpler with an easy-to-use API that works across a multitude of browsers."  
