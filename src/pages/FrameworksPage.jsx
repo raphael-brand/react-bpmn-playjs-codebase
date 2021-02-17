8e5d6a8c8e698ecde10dd2bd88ad0d3be0859129
Raphael Brand <info@raphaelbrand.de>
2021-02-17 21:51:28 +0100

    search input field

--- src/pages/FrameworksPage.jsx
+++ src/pages/FrameworksPage.jsx
@@ -1,9 +1,41 @@
-const FrameworksPage = () => {
-  // We can call useParams() here to get the params,
-  // or in any child element as well!
- // ...
- let pagename = "Frameworks";
-  return `<h1>Hello World! with ${pagename}</h1>`;
+
+import React, { Component, createRef } from 'react';
+import items from './items';
+
+
+class SearchInput extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.search = createRef();
+  } 
+  
+  componentDidMount() {
+    this.search.current.addEventListener('focus', x => x.target.value = '');
+    this.search.current.addEventListener('blur', x => x.target.value = 'enter search term');
+    
+  }
+  
+  render() {     
+     return (
+       <input type="text" ref={this.search} defaultValue="enter search term"/>
+     );
+  }
+  
+  
+};
+
+
+class FrameworksPage extends Component {
+
+ 
+  render() {
+    return (<>
+     <h1>Frameworks</h1>
+     <SearchInput />
+    </>);
+  }
+
 }
 
 export default FrameworksPage;
