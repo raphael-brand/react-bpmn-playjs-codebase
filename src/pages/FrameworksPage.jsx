55b9447bb4b24c7260d8b7648eab579144d153d5
Raphael Brand <info@raphaelbrand.de>
2021-02-18 15:07:18 +0100

    frameworks search input handlers

--- src/pages/FrameworksPage.jsx
+++ src/pages/FrameworksPage.jsx
@@ -8,21 +8,58 @@ class SearchInput extends Component {
   constructor(props) {
     super(props);
     this.search = createRef();
-  } 
+    this.default_text = 'enter search term';
+    this.onInputEnd = this.onInputEnd.bind(this);
+    this.storeSearchTerm = this.storeSearchTerm.bind(this);
+    this.state = {current_search:'', search_term:''}
+  }
   
   componentDidMount() {
     this.search.current.addEventListener('focus', x => x.target.value = '');
-    this.search.current.addEventListener('blur', x => x.target.value = 'enter search term');
-    
+    this.search.current.focus();
+    this.search.current.addEventListener('blur', this.storeSearchTerm);
+  }
+ 
+  onInputEnd(event) {
+    this.setState({ search_term: event.target.value });
+  }
+  
+  startSearch() {
+  //  window.location.hash = '#'+this.state.current_search;
+  }
+  
+  resetSearchInput() {
+      
+      this.setState({search_term: ''});
+//      alert(this.state.current_search);
+
+  }
+  
+  storeSearchTerm(event) {
+
+   if(this.state.current_search ==='') {
+     
+      event.target.value = 'test---'+this.state.search_term+'---test';
+      this.setState({current_search: this.state.search_term});
+  
+   }
+   else {
+     this.startSearch();
+   }
+   
+   if(event.target.value === this.default_text || event.target.value == '') {
+
+      this.resetSearchInput();
+
+  }
+   
   }
   
   render() {     
      return (
-       <input type="text" ref={this.search} defaultValue="enter search term"/>
+       <input type="text" ref={this.search} defaultValue={this.default_text} onChange={this.onInputEnd}/>
      );
   }
-  
-  
 };
 
 
