deb20ef07db27eab35c1bb254b74624cd44ff2a0
Raphael Brand <info@raphaelbrand.de>
2021-02-25 10:56:51 +0000

    Initialize Project using Create React App

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,49 @@
-import React from "react";
-// import route stuff
+import React, { useState, useEffect } from "react";
+import logo from "./cra-logo.png";
+import "./App.css";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+// import { SetCurrentUser } from "./redux/user/user.actions";
+// import { selectCurrentUser } from "./redux/user/user.selector";
+import { createStructuredSelector } from "reselect";
+import Header from "./components/Header";
+const App = (props) => {
+  //these variables are used for firebase signin(good examples of using redux to pass the user as a prop to the app.jsx)
+  // const { setCurrentUser } = props;
+  // const unsubscribeFromAuth = null;
 
-import {
- Route,
-  Switch,
- useParams
-} from 'react-router-dom'
+  return (
+    <div className="App">
+      {<><Header />
+      <Switch>
+        <Route exact path="/" component={} />
+        <Route exact path="/" component={} />
+        <Route exact path="/" component={} />
+        <Route exact path="/signin" />
+      </Switch></>}
 
-// import Navigation component
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <br />
+        <p>
+          Welcome to my <code>CRA-Template</code>
+        </p>
+      </header>
+    </div>
+  );
+};
 
-import Navigation from './pages/Navigation.jsx';
-// import Homepage component
-import HomePage from 'home';
-import FrameworksPage  from 'frameworks';
-// import Modeler page component
-import ModelerPage from 'modeler';
-
-export default () => {
-  return (<>
-    {/* render a navigation component */}
-   <Navigation />
-   <hr />
-    {/* render routing logic of react-router-dom */}
-   <Switch>
-    <Route exact path="/">
-      <HomePage />
-    </Route> 
-    <Route path="/frameworks">
-      <FrameworksPage />
-    </Route> 
-    <Route path="/modeler">
-      <ModelerPage />
-    </Route> 
-   </Switch>
-  
-  </>)
+{
+  /* this is how we pass props with redux to our App.jsx */
 }
+{
+  /* const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(SetCurrentUser(user)),
+}); */
+}
+
+export default App;
