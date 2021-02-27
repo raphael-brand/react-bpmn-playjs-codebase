7a56decbdc120dc1b64b284e48441a04c2720569
Raphael Brand <info@raphaelbrand.de>
2021-02-27 16:40:27 +0000

    navigation component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from "react";
-
+import React from "react";
+import Navigation from './../pages/Navigation';
 const Header = (props) => {
   return (
-    <div>
-      <h1>Header Section</h1>
-    </div>
+      <Navigation />
   );
 };
 
