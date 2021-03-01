e682fd13ade385f0705bf15ff2c073ee5a55a322
Raphael Brand <info@raphaelbrand.de>
2021-03-01 14:46:31 +0000

    frameworks component test

---
+++ src/__test__/Frameworks.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Enzyme, {shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import FrameworksPage from './../components/FrameworksPage'
+Enzyme.configure({adapter: new Adapter()});
+
+it('renders without crashing', () => {
+    
+    let app = shallow(<FrameworksPage />);
+});
