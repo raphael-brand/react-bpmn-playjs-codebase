dc26e026fd987591686a4d1e6b5635ee5027c24c
Raphael Brand <info@raphaelbrand.de>
2021-03-01 15:50:02 +0000

    create result list attributes and add tests

--- src/__test__/ResultList.test.js
+++ src/__test__/ResultList.test.js
@@ -2,13 +2,11 @@ import React from "react";
 
 import Enzyme, {shallow} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-
-import App from './App';
-import FrameworksPage from './components/FrameworksPage';
-
+import ResultList from './../components/ResultList';
+import items from './../components/items'
 Enzyme.configure({adapter: new Adapter()});
 
-
 it('renders without crashing', () => {
-    let app = shallow(<FrameworksPage/>)
+    
+    let app = shallow(<ResultList items={items} />);
 });
