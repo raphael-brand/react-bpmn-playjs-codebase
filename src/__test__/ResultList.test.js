37cb29dbc1b6f9819505afecd9fd42b0064c8c53
Raphael Brand <info@raphaelbrand.de>
2021-03-05 18:53:54 +0000

    index on dev-files: bb943e8 call filter method on change

--- src/__test__/ResultList.test.js
+++ src/__test__/ResultList.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Enzyme, {shallow} from "enzyme";
+import Enzyme, {shallow, render} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import ResultList from './../components/ResultList';
 import items from './../components/items'
@@ -8,5 +8,18 @@ Enzyme.configure({adapter: new Adapter()});
 
 it('renders without crashing', () => {
     
-    let app = shallow(<ResultList items={items} />);
+    let app = shallow(<ResultList items={items} filter={""}/>);
 });
+
+
+it('creates an array from a search result', () => {
+
+		let filter = "";
+
+		let resultList = render(<><ResultList items={items} filter={filter}  /></>);
+
+		filter = "React";
+
+
+
+})
--- src/components/ResultList.jsx
+++ src/components/ResultList.jsx
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from "prop-types";
 import ResultListItem from "./ResultListItem";
 
 const ResultList = (props) => {
-    let result = [];
-    const input = props.items;
-    input.forEach((ds) => {
-        let _data = { 
-            name: Object.keys(ds),
-            text: Object.values(ds)
-        }
-        result.push(<ResultListItem
-            name={_data.name}
-            text={_data.text}
-        />);
-        
-    });
-    return result;
+
+
+  let result = [];
+  const { filter, items } = props;
+  let filteredItems = filter == '' ? items : [];
+
+  useEffect(() => {
+
+	  filteredItems = items.find(
+
+		  (values, keys) => {
+
+		  let result = Object.keys(values)[0].match(filter);
+
+		  // found match
+		  let name = result && result.input
+		  result =  Object.values(values)[0].match(filter)
+		  let text = result && result.input
+		  return name || text
+		  });
+
+	  }, [filter]);
+
+	  filteredItems.forEach((ds) => {
+
+		let _data = { 
+			name: Object.keys(ds),
+			text: Object.values(ds)
+		}
+
+		result.push(
+		  <ResultListItem
+			name={_data.name}
+			text={_data.text}
+		  />
+		);
+
+	  });
+
+	  return result;
+
 }
 
 export default ResultList;
 
+
+
 ResultList.propTypes = {
-    items: PropTypes.array.isRequired
+		items: PropTypes.array.isRequired,
+		filter: PropTypes.string.isRequired
 };
+
